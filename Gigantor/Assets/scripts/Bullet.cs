3e7b836a93adfbdb41d7018c361439d21595370c
gallaid <smoot.spencer@gmail.com>
2016-01-31 10:59:54 -0800

    WeaponWorking

---
+++ Gigantor/Assets/scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    int baseDamage = 1;
+    float Distance;
+    int TotalDamage;
+    bool fired = false;
+    Vector3 startPoint;
+	// Use this for initialization
+	void Start ()
+    {
+	startPoint = gameObject.transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fired) {
+        DistanceCheck();
+    }
+
+	}
+    public void Discharged(int payLoad,float distance)
+    {
+        Distance = distance;
+        fired = true;
+    }
+    public void DistanceCheck()
+    {
+        if (Distance <= Vector2.Distance(startPoint, gameObject.transform.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Gigantor/Assets/scripts/WeaponsSystem.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WeaponsSystem : MonoBehaviour {
+    private int payLoad;
+    public Bullet bullet;
+    public float fireRate=0.5f;
+    public float speedOfProjectile;
+    public float rechargeSpeedFront;
+    public float nextTime = 0.0f;
+    private float rechargeSpeed;
+
+    private float charge;
+    
+    
+
+    public Slider DistanceSlider;
+
+	// Use this for initialization
+	void Start () {
+        payLoad = 1;
+        
+	}
+	// Update is called once per frame
+	void Update () {
+        rechargeSpeed =rechargeSpeedFront /100;
+       
+            ChargeShot();
+        
+        
+        
+        //DistanceSlider.value += rechargeSpeed;
+        
+	}
+    void fire(float charge)
+    {
+        if (CanFire())
+        {
+            Bullet projectile = Instantiate(bullet, gameObject.transform.position+Vector3.up*3, Quaternion.identity) as Bullet;
+            projectile.Discharged(payLoad, charge);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+            rb.AddRelativeForce(Vector2.up *speedOfProjectile);
+            
+        }    
+    }
+    bool CanFire()
+    {
+        if (Time.time > nextTime)
+        {
+            nextTime = Time.time + fireRate;
+            return true;
+        }
+        else return false;
+    }
+    void ChargeShot()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            charge += rechargeSpeed * 2;
+            DistanceSlider.value = charge;
+            charge = Mathf.Clamp(charge, 1, 5);
+        }else if (Input.GetButtonUp("Fire1"))
+        {
+            fire(charge);
+        }
+        else
+        {
+            Recharge();
+        }
+    }
+    void Recharge()
+    {
+        charge -= rechargeSpeed;
+        DistanceSlider.value = charge;
+    }
+
+}
