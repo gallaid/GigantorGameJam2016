7a2922e870441d7ef564c6824dc4f7774263b83c
Daniel Shea <sheadj@usc.edu>
2016-01-31 12:02:10 -0800

    synch

--- Gigantor/Assets/scripts/playerControls.cs
+++ Gigantor/Assets/scripts/playerControls.cs
@@ -1,28 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerControls : MonoBehaviour {
+public class playerControls : MonoBehaviour
+{
     public float forwardSpeed = 0.05f;
     public float reverseSpeed = 0.02f;
     public float turnSpeed = 1f;
     public float speedMod = 1f;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
+    private Vector3 angle = Vector3.up;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    /*
+    void Update()
+    {
+        if (Mathf.Abs(angle.x) > Mathf.Abs(angle.y))
+        {
+            if (angle.x > 0)
+                GetComponent<Animator>().Play("playerWalkRight");
+
+            if (angle.x < 0)
+                GetComponent<Animator>().Play("playerWalkLeft");
+        }
+
+        if (Mathf.Abs(angle.x) < Mathf.Abs(angle.y))
+        {
+            if (angle.y > 0)
+                GetComponent<Animator>().Play("playerWalkUp");
+
+            if (angle.x < 0)
+                GetComponent<Animator>().Play("playerWalkDown");
+        }
+    }
+
 	void FixedUpdate () {
         //turn when rotating
-        transform.Rotate(new Vector3(0, 0, -turnSpeed * Input.GetAxis("Horizontal")));
+        //transform.Rotate(new Vector3(0, 0, -turnSpeed * Input.GetAxis("Horizontal")));
+        angle = Quaternion.Euler(0, 0, -turnSpeed * Input.GetAxis("Horizontal")) * angle;
+
 
         //if speed is forward
         if (Input.GetAxis("Vertical") > 0){
-            transform.position += transform.up * forwardSpeed * speedMod * Input.GetAxis("Vertical");
+            transform.position += angle * forwardSpeed * speedMod * Input.GetAxis("Vertical");
 
         } else if (Input.GetAxis("Vertical") < 0){
-            transform.position += transform.up * reverseSpeed * speedMod * Input.GetAxis("Vertical");
+            transform.position += angle * reverseSpeed * speedMod * Input.GetAxis("Vertical");
 
         }
+    }
+    */
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * forwardSpeed * speedMod;
     }
 }
